6dde21c894211d158b58a3c365af01e7605e3fa5
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2024-04-05 19:04:53 -0500

    A lot for navigation
    I added javascript to changed class nojs to hasjs. I made the navigation pop out whenever the burger is clicked and retract when it is clicked again. Worked on the navigation so that the background would be all black and made a special condition for when the navigation is in the downloads page so that it goes down all the way to the bottom of the screen since that one is bigger than the index. I spent most of the day doing this. Making websites from scratch is painful yet rewarding. I'm going a little insane.

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,20 @@
+document.documentElement.classList.remove('nojs');
+document.documentElement.classList.add('hasjs');
+
+const burgerMenu = document.querySelector('.burger');
+burgerMenu.addEventListener('click', function() {
+    menu(this);
+});
+
 document.addEventListener("DOMContentLoaded", function() {
-    var element = document.querySelector('.nojs');
-    element.classList.remove('nojs');
-    element.classList.add('js');
     const form = document.querySelector('form');    
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-
         const email = form.elements['email'].value.trim();
         const username = form.elements['username'].value.trim();
         const password = form.elements['password'].value.trim();
         const confirmPassword = form.elements['confirm_password'].value.trim();
         const ToS = form.elements['ToS'].checked;
-
         if (email === '' || username === '' || password === '' || confirmPassword === '' || !ToS) {
             alert('Please fill out all fields and agree to the terms of service.');
             return;
@@ -30,6 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 function menu(x){
       x.classList.toggle("change");
+      const elementsToToggle = document.querySelectorAll('.burgered');
+      elementsToToggle.forEach(element =>{
+        element.classList.toggle("change");
+      });
     }
 document.getElementById("toDownload").addEventListener("click", function(){
         window.location.href = "downloads.html";
