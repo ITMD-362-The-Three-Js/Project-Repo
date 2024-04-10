79c7f6c34b6d54b46d32c030d6749d4a85e09b85
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2024-04-10 18:51:18 -0500

    Spacing fix for js
    Fixes the indentation in the js code to make it two spaces.

--- javascript/app.js
+++ javascript/app.js
@@ -3,41 +3,40 @@ document.documentElement.classList.add('hasjs');
 
 const burgerMenu = document.querySelector('.burger');
 burgerMenu.addEventListener('click', function() {
-    menu(this);
+  menu(this);
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector('form');    
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const email = form.elements['email'].value.trim();
-        const username = form.elements['username'].value.trim();
-        const password = form.elements['password'].value.trim();
-        const confirmPassword = form.elements['confirm_password'].value.trim();
-        const ToS = form.elements['ToS'].checked;
-        if (email === '' || username === '' || password === '' || confirmPassword === '' || !ToS) {
-            alert('Please fill out all fields and agree to the terms of service.');
-            return;
-        }
+  const form = document.querySelector('form');    
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = form.elements['email'].value.trim();
+    const username = form.elements['username'].value.trim();
+    const password = form.elements['password'].value.trim();
+    const confirmPassword = form.elements['confirm_password'].value.trim();
+    const ToS = form.elements['ToS'].checked;
+    if (email === '' || username === '' || password === '' || confirmPassword === '' || !ToS) {
+      alert('Please fill out all fields and agree to the terms of service.');
+      return;
+    }
+    if (password !== confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+    }
 
-        if (password !== confirmPassword) {
-            alert('Passwords do not match.');
-            return;
-        }
-
-        // Here you can add additional validation or form submission logic
-        
-        // If all validations pass, you can submit the form
-        form.submit();
-    });
+    // Here you can add additional validation or form submission logic
+     
+    // If all validations pass, you can submit the form
+    form.submit();
+  });
 });
 function menu(x){
-      x.classList.toggle("change");
-      const elementsToToggle = document.querySelectorAll('.burgered');
-      elementsToToggle.forEach(element =>{
-        element.classList.toggle("change");
-      });
-    }
+  x.classList.toggle("change");
+  const elementsToToggle = document.querySelectorAll('.burgered');
+  elementsToToggle.forEach(element =>{
+    element.classList.toggle("change");
+  });
+}
 document.getElementById("toDownload").addEventListener("click", function(){
-        window.location.href = "downloads.html";
-    });
+  window.location.href = "downloads.html";
+});
